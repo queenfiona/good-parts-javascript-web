3997ed518d10ab7f9db84d898dc29dc5ca0ad996
queenfiona <fionaitya@gmail.com>
2019-10-07 21:06:44 +0300

    Extend the array to get the last element

--- js-objects-and-prototypes/script.js
+++ js-objects-and-prototypes/script.js
@@ -14,3 +14,14 @@ let cat = new Cat('Fluffy', 'White');
 
 display(cat.name,cat.color);
 cat.speak()
+
+display(Object.getOwnPropertyDescriptor(cat,'name'));
+
+let array1 = ['red', 'blue', 'green']
+// Define last property to extend array1- get last element
+Object.defineProperty(array1, 'last', { get: function() {
+	return this[this.length-1];
+}});
+
+let last = array1.last;
+display(last);
